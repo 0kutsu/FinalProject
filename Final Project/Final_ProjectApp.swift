92465113e0ba0c612dfc49bfe5eb66d48e170ac8
Mati Okutsu <s027891@students.lmsd.org>
2024-03-08 07:45:14 -0500

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Mati Okutsu (student LM) on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
