77416a6bbe098c26fa888db7bcdb552e4524d676
Jamie Oswald <jamieli006@hotmail.com>
2024-03-20 09:19:10 -0400

    message ui stuff

---
+++ Final Project/ChatViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  ChatViewModel.swift
+//  Final Project
+//
+//  Created by Jamie Oswald (student LM) on 3/20/24.
+//
+
+import Foundation
+
+class ChatViewModel: ObservableObject {
+    @Published var messages = [Message]()
+    
+    @Published var mockData = [
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date()),
+        Message(userUid: "12345", text: "text message", photoURL: "", createdAt: Date())
+    ]
+    func sendMessage(text: String) {
+        print(text)
+    }
+}
